7c58d804927066b7cbd42bbd73d0e9209850a17f
kss306 <elias.funke30@gmail.com>
2025-11-18 17:48:12 +0100

    updated solution sodass sie automatisch die r2modman files ausgibt

--- BonkersLib/CHANGELOG.md
+++ BonkersLib/CHANGELOG.md
@@ -1,3 +1,7 @@
+### v1.0.1
+
+- Changed the way the "IsInGame" State would be triggered from timer based to scene checks for certain interactables
+
 ### v1.0.0
 
 - Initial release.
--- MegaBonkPlusMod/Core/PluginInfo.cs
+++ MegaBonkPlusMod/Core/PluginInfo.cs
@@ -4,5 +4,5 @@ public static class PluginInfo
 {
     public const string PLUGIN_GUID = "com.kss.megabonkplus";
     public const string PLUGIN_NAME = "MegaBonkPlus";
-    public const string PLUGIN_VERSION = "0.9.0";
+    public const string PLUGIN_VERSION = "0.9.1";
 }
